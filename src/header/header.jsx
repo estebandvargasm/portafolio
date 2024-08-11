40360dac51ca05f6f94d3e696a7b01fb563e4c60
estebandvargasm <estebandvargasm@outlook.com>
2024-08-11 17:43:34 -0500

    Update header content and fix typo in welcome message

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     <header className="header">
       <div className="header-content">
         <div className="header-text">
-          <h1 className="header-title">Wellcome</h1>
+          <h1 className="header-title">Welcome</h1>
           <p className="header-subtitle">
-            I'm Esteban, a frontend developer passionate about designing and creating professional websites. Let's talk and make your ideas a reality.
+            My name is Esteban Vargas, I'm a front-end developer passionate about designing and creating professional websites. Let's talk and make your ideas a reality.
           </p>
           <div className="header-buttons">
             <button className="header-button contact-button">
@@ -18,9 +18,6 @@ const Header = () => {
               Download CV
             </button>
           </div>
-        </div>
-        <div className="header-image">
-          <img src="/logo.png" alt="Logo" className="profile-image" />
         </div>
       </div>
     </header>
